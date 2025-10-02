a707cb767eca450f3ccd1d2d0b1ef1fd67a6bd3e
Enci <ench0086@gmail.com>
2025-10-02 23:57:40 +0100

    test for get all

--- tests/Api.tests/Controllers/StockControllerTests.cs
+++ tests/Api.tests/Controllers/StockControllerTests.cs
@@ -1,66 +1,42 @@
-using NUnit.Framework;
-using Moq;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using api.Controllers;
-using api.Interfaces;
-using api.Models;
-using api.Dtos.Stock;
-using api.Helpers;
-
-namespace api.Tests
+﻿[TestFixture]
+public class StockControllerTests
 {
-    [TestFixture]
-    public class StockControllerTests
+    private Mock<IStockRepository> _repoMock;
+    private StockController _controller;
+
+    [SetUp]
+    public void Setup()
     {
-        private Mock<IStockRepository> _mockRepo;
-        private StockController _controller;
+        _repoMock = new Mock<IStockRepository>();
+        _controller = new StockController(_repoMock.Object);
+    }
 
-        [SetUp]
-        public void Setup()
+    [Test]
+    public async Task GetAll_WhenCalled_ReturnsOkWithStockDtos()
+    {
+        // Arrange
+        var fakeStocks = new List<Stock>
         {
-            _mockRepo = new Mock<IStockRepository>();
-            _controller = new StockController(_mockRepo.Object);
-        }
+            new Stock { Id = 1, Name = "Apple" },
+            new Stock { Id = 2, Name = "Tesla" }
+        };
 
-        [Test]
-        public async Task GetAll_ReturnsOkWithStocks()
-        {
-            // Arrange
-            var mockStocks = new List<Stock>
-            {
-                new Stock { Id = 1, Name = "Test Stock", Quantity = 10 },
-                new Stock { Id = 2, Name = "Another Stock", Quantity = 20 }
-            };
+        // Setup repo to return the fake stocks, no matter the QueryObject
+        _repoMock
+            .Setup(r => r.GetAllAsync(It.IsAny<QueryObject>()))
+            .ReturnsAsync(fakeStocks);
 
-            _mockRepo.Setup(r => r.GetAllAsync(It.IsAny<QueryObject>())).ReturnsAsync(mockStocks);
+        // Act
+        var result = await _controller.GetAll(new QueryObject());
 
-            // Act
-            var result = await _controller.GetAll(new QueryObject());
+        // Assert
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        var returned = okResult.Value as List<StockDto>;
+        Assert.AreEqual(2, returned.Count);
+        Assert.AreEqual("Apple", returned[0].Name);
 
-            // Assert
-            var okResult = result as OkObjectResult;
-            Assert.IsNotNull(okResult);
-            var returnedStocks = okResult.Value as List<StockDto>;
-            Assert.AreEqual(2, returnedStocks.Count);
-        }
-
-        [Test]
-        public async Task GetById_StockExists_ReturnsOk()
-        {
-            // Arrange
-            var stock = new Stock { Id = 1, Name = "Test Stock", Quantity = 10 };
-            _mockRepo.Setup(r => r.GetByIdAsync(1)).ReturnsAsync(stock);
-
-            // Act
-            var result = await _controller.GetById(1);
-
-            // Assert
-            var okResult = result as OkObjectResult;
-            Assert.IsNotNull(okResult);
-            var returnedStock = okResult.Value as StockDto;
-            Assert.AreEqual("Test Stock", returnedStock.Name);
-        }
+        // Also verify interaction if you want
+        _repoMock.Verify(r => r.GetAllAsync(It.IsAny<QueryObject>()), Times.Once);
     }
 }
